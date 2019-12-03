916cb0dcfc1b0d85888613c06e93f2d05e1a3bf4
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-03 13:48:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+var num1 = 31
+var num2 = 2
+var multiply =  num1 * num2
+var random = Math.floor( Math.random() * 1000 ) + 1
+var mod = 16 % 6
+var max = Math.max( 5, 10, 15, 20 )
